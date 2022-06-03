9ef5bc797797b0c1dc1c4a0c0342d1468eccf247
Hana Jirovska <h.jirovska@student.tudelft.nl>
2022-06-03 11:02:00 +0200

    Add stage for downloading data files
    - Update the download script
    - Add another stage download_data
    - Update dependencies for create_model

--- src/data/download_files.py
+++ src/data/download_files.py
@@ -1,18 +1,22 @@
 """
 Module responsible for downloading data files.
 """
-
 import yaml
 import gdown
 
 
 def download_files():
+    """
+    Downloads three data files (train, test, validation) based on the specified
+    links given in `config.yaml`.
+    """
 
-    with open('../../config.yaml') as file:
+    with open('././config.yaml', 'r', encoding="UTF-8") as file:
         try:
             config = yaml.safe_load(file)
             for data in ["train", "test", "validation"]:
-                gdown.download(config[f"{data}_data"], f"../../data/{data}.tsv", quiet=True, fuzzy=True)
+                gdown.download(config[f"{data}_data"], f"././data/{data}.tsv",
+                               quiet=True, fuzzy=True)
         except yaml.YAMLError as exc:
             print(exc)
 
